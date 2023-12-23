f01fb99bc75f7e3b6a878ad485a6517d87d4fcab
Stephen Martinez <122754065+stephenm7777@users.noreply.github.com>
2023-12-23 05:26:02 -0800

    main update

--- recurrentTypeNetwork/src/main/java/org/example/Main.java
+++ recurrentTypeNetwork/src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Gateway to spark
+        SparkSession spark = SparkSession
+                .builder()
+                .master("local[*]")
+                .config("spark.sql.warehouse.dir", "/tmp/spark")
+                .appName("SurvivalPredictionMLP")
+                .getOrCreate();
+        //Reading the training set and getting a glimpse of it
+        Dataset<Row> df = spark.sqlContext()
+                .read()
+                .format("com.databricks.spark.csv")
+                .option("header", "true")
+                .option("inferSchema", "true")
+                .load("data/train.csv");
+        df.show();
     }
 }
